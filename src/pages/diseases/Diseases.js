3a06193a6bf0b1c00afde28faa01bf2d89b640e7
EdoSalas <esalas95@outlook.com>
2021-10-21 17:21:05 -0600

    BACK | Borrado del back

--- src/pages/diseases/Diseases.js
+++ src/pages/diseases/Diseases.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Regions from "../../components/Regions/regions";
 
 const Diseases = () => (
   <div className="grid">
     <h1>Enfermedades</h1>
-    {/* <Regions/> */}
   </div>
 );
 
