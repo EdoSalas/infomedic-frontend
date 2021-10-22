0d68f275ce99207a2c97f229f32960c70135b606
EdoSalas <esalas95@outlook.com>
2021-10-21 19:41:31 -0600

    BACK | Add services

---
+++ src/services/regions.services.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const url = "https://esalas.me/api";
+
+export const getAll = async () => {
+    try {
+        const response = await axios.get(`${url}/regions`);
+        return await response.data;
+    } catch (error) {
+        console.log("Error: ", error);
+        return error;
+    }
+};
